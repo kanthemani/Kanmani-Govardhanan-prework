15a6955413970a71b86acfa237ddf403dadd8f57
Kanmani <govardh3@msu.edu>
2021-02-13 11:15:54 -0600

    initial

---
+++ Javascript_Basics_Assessment/activity-2.js
@@ -0,0 +1,7 @@
+const userInput = prompt('What is your name?');
+if (userInput.length>4) {
+    alert('Name is greater than 4 characters');
+}
+else {
+    alert('Name is less than 4 characters');
+}
---
+++ Javascript_Basics_Assessment/activity-3.js
@@ -0,0 +1,16 @@
+const nameList = ['George', 'Lisa', 'Glenna'];
+for (let i = 0; i < 3; i++){
+    const name = prompt('Enter a name');
+    nameList.push(name);
+    const nameList = [];
+
+nameList.push(name1);
+nameList.push(name2);
+nameList.push(name3);
+}
+
+for (let i = 0; i < nameList.length; i++){
+    const name = nameList[i];
+    alert(name);
+  }
+  
