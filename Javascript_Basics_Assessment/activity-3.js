1a168c8505469da3f546dfb83d18dafd5fc53650
Kanmani <govardh3@msu.edu>
2021-02-14 12:04:52 -0600

    done

--- Javascript_Basics_Assessment/activity-3.js
+++ Javascript_Basics_Assessment/activity-3.js
@@ -1,16 +1,17 @@
-const nameList = ['George', 'Lisa', 'Glenna'];
-for (let i = 0; i < 3; i++){
-    const name = prompt('Enter a name');
-    nameList.push(name);
-    const nameList = [];
 
+const name1 = prompt('enter a student name');
+const name2 = prompt('enter a student name');
+const name3 = prompt('enter a student name');
+
+const nameList = []
 nameList.push(name1);
 nameList.push(name2);
 nameList.push(name3);
+for (let i = 0; i < 3; i++){
+    const name = prompt('Enter a name');
+    nameList.push(name);
 }
-
-for (let i = 0; i < nameList.length; i++){
-    const name = nameList[i];
-    alert(name);
-  }
-  
+var arraylength=nameList.length
+for (var i = 0; i < arraylength; i++) {
+    console.log(nameList[i]);
+}
