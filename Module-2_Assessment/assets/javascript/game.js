fb69aa5bf7d5f842ae128624bc6a0a66b66955bd
Kanmani <govardh3@msu.edu>
2021-02-15 10:30:24 -0600

    commit

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,29 +1,99 @@
-var $ = function (id) {
-    return document.getElementById(id);
-}
 
-var PossibleWords=["apple", "banana", "watermelon", "cantaloupe", "honeydew", "jackfruit", "papaya", "dragonfruit", 
+
+    var PossibleWords=["apple", "banana", "watermelon", "cantaloupe", "honeydew", "jackfruit", "papaya", "dragonfruit", 
             "pineapple", "durian", "blood orange", "strawberry", "apricot", "kumquat", "pomegranate"];
 
-var word = PossibleWords[Math.floor(Math.random() * PossibleWords.length)];     
-var answer= PossibleWords[word];
-var myLength = answer.length;
-var display=[myLength];
-var win = myLength;
-var letters = answer.split('');
-var attemptsLeft=9;
-var output="";
-
-var setup = function()
-{
-    for (var i=0; i<answer.length; i++)
-    {
-        display[i] = "_";
-        output = output + display[i];
+    const GuessesLeft = 7
+    var pauseGame = false
+        
+    var guessedLetters = []
+    var guessingWord = []
+    var correctWord
+    var Guess
+    var wins = 0
+        
+    resetGame()
+        
+    document.onkeypress = function(event) {
+      
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+            }
+        }
+        
+    
+    function checkForLetter(letter) {
+        var foundLetter = false
+        
+      
+        for (var i=0, j= correctWord.length; i<j; i++) {
+             if (letter === correctWord[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                
+                if (guessingWord.join("") === correctWord) {
+                   
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame,5000)
+                }
+            }
+        }
+        
+        if (!foundLetter) {
+           
+            if (!guessedLetters.includes(letter)) {
+               
+                guessedLetters.push(letter)
+                
+                Guess--
+            }
+            if (Guess === 0) {
+                
+                guessingWord = correctWord.split()
+                pauseGame = true
+                setTimeout(resetGame, 5000)
+            }
+        }
+        
+         updateDisplay()
+        
     }
-    document.getElementById("PossibleWords").innerHTML = output;
-    output ="";
-}
-
-
-
+    
+    function isAlpha (ch){
+        return /^[A-Z]$/i.test(ch);
+    }
+        
+    function resetGame() {
+        Guess = GuessesLeft
+        pauseGame = false
+        
+        
+        correctWord = PossibleWords[Math.floor(Math.random() * PossibleWords.length)].toUpperCase()
+        console.log(correctWord)
+        
+        
+        guessedLetters = []
+        guessingWord = []
+        
+        
+        for (var i=0, j=correctWord.length; i < j; i++){
+            
+            if (correctWord[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+        
+        
+        updateDisplay()
+    }
+        
+    function updateDisplay () {
+        document.getElementById("totalWins").innerHTML = wins
+        document.getElementById("currentWord").innerHTML = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerHTML = Guess
+        document.getElementById("guessedLetters").innerHTML =  guessedLetters.join(" ")
+    }
